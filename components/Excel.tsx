f6ea8301ade9b396731c2094c0012b78cb86341e
Sravani Dande <sravanidnd89@gmail.com>
2021-12-16 00:04:29 +0530

    make excel headers support sorting (asc,desc)

--- components/Excel.tsx
+++ components/Excel.tsx
@@ -1,31 +1,66 @@
-import { Table, Th, Thead, Tr, Tbody, Td } from '@chakra-ui/react'
+import { Table, Th, Thead, Tr, Tbody, Td, IconButton } from '@chakra-ui/react'
+import { BiSortDown, BiSortUp, BiSortAlt2 } from 'react-icons/bi'
 import React from 'react'
+
+export type Sort = 'asc' | 'desc'
 
 interface ExcelViewProps {
   headers: string[]
   data: string[][]
-  onSortHeader(idx: number): void
+  sort?: Sort
+  selectedIdx?: number
+  onSortHeader(idx: number, sort?: Sort): void
+}
+
+interface IconViewProps {
+  sort?: Sort
+}
+
+export const IconView = ({ sort }: IconViewProps) => {
+  return sort === 'asc' || sort === undefined ? (
+    <IconButton size="xs" aria-label="asc" icon={<BiSortUp />} />
+  ) : (
+    <IconButton size="xs" aria-label="desc" icon={<BiSortDown />} />
+  )
 }
 
 export const ExcelView: React.FC<ExcelViewProps> = ({
   headers,
   data,
+  sort,
+  selectedIdx,
   onSortHeader,
 }) => {
   return (
     <Table>
       <Thead>
         <Tr>
-          {headers.map((h, idx) => (
-            <Th key={idx} onClick={() => onSortHeader(idx)}>
-              {h}
-            </Th>
-          ))}
+          {headers.map((h, idx) => {
+            return (
+              <Th
+                key={idx}
+                onClick={() => {
+                  console.log(idx, 'idx')
+                  onSortHeader(idx, sort)
+                }}
+              >
+                {h}{' '}
+                {selectedIdx === idx ? (
+                  <IconView sort={sort} />
+                ) : (
+                  <IconButton
+                    size="xs"
+                    aria-label="sortable"
+                    icon={<BiSortAlt2 />}
+                  />
+                )}
+              </Th>
+            )
+          })}
         </Tr>
       </Thead>
       <Tbody>
         {data.map((books, idx) => {
-          // console.log(books)
           return (
             <Tr key={idx}>
               {books.map((b, idx) => (
--- pages/excel/index.tsx
+++ pages/excel/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ExcelView } from '../../components/Excel'
+import { ExcelView, Sort } from '../../components/Excel'
 import { excelData } from '../../components/data'
 import produce from 'immer'
 
@@ -13,26 +13,31 @@ const cmp = (x: any, y: any) => {
   return 0
 }
 
+const revCmp = (x: any, y: any) => cmp(y, x)
+
 const ExcelPage = () => {
-  const [headerIndex, setHeaderIndex] = React.useState<number | undefined>(
-    undefined,
-  )
+  const [headerIndex, setHeaderIndex] = React.useState<number | undefined>()
+  const [sort, setSort] = React.useState<Sort | undefined>()
 
   React.useEffect(() => {
     if (headerIndex === undefined) {
       return
     }
 
+    const fn = sort === 'desc' ? revCmp : cmp
     set(
       produce(data, draft => {
-        draft.sort((x, y) => cmp(x[headerIndex], y[headerIndex]))
+        draft.sort((x, y) => fn(x[headerIndex], y[headerIndex]))
       }),
     )
-  }, [headerIndex])
+  }, [headerIndex, sort])
 
   const [data, set] = React.useState<string[][]>(excelData.data)
 
-  const handleSort = (index: number) => {
+  const handleSort = (index: number, sort: Sort | undefined) => {
+    const order = index !== headerIndex || sort === 'desc' ? 'asc' : 'desc'
+    setSort(order)
+
     setHeaderIndex(index)
   }
 
@@ -40,6 +45,8 @@ const ExcelPage = () => {
     <ExcelView
       headers={excelData.headers}
       data={data}
+      sort={sort}
+      selectedIdx={headerIndex}
       onSortHeader={handleSort}
     />
   )
